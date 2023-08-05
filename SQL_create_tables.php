ad79f03631c3cb8c6d8b193714461436ea2f3024
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-08-05 18:37:07 +0200

    ok

--- SQL_create_tables.php
+++ SQL_create_tables.php
@@ -31,3 +31,14 @@ CREATE TABLE settings (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+CREATE TABLE settings (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    setting_name VARCHAR(50) NOT NULL,
+    setting_value VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
